a95dc835a1a532ca2b334ad30701192b23e58d3e
ryantido34 <ryantido34@gmail.com>
2025-10-14 21:18:30 +0100

    design:hero section component, Gsap inteh=gration, update global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Hero } from "@/components/sections/Hero";
 import React from "react";
 
 export default function MainPage() {
   return (
     <>
-      <main className="container mx-auto min-h-dvh px-4 md:px-6 lg:px-8">
-        Hello World!
+      <main className="relative min-h-dvh overflow-x-hidden">
+        <Hero />
       </main>
       <footer></footer>
     </>
