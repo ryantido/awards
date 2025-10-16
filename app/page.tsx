6dbfad0167f68939e25c8f37b3a1ee02f2917960
ryantido34 <ryantido34@gmail.com>
2025-10-16 12:57:50 +0100

    design:design story section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/sections/About";
 import { Feature } from "@/components/sections/Feature";
 import { Hero } from "@/components/sections/Hero";
+import { Story } from "@/components/sections/Story";
 import React from "react";
 
 export default function MainPage() {
@@ -10,6 +11,7 @@ export default function MainPage() {
         <Hero />
         <About />
         <Feature />
+        <Story />
       </main>
       <footer></footer>
     </>
